bb4b9001adf607a246935a82206e42294271dfeb
pm-wobbuffet <6683213+pm-wobbuffet@users.noreply.github.com>
2024-06-19 20:54:17 -0400

    demo echo code for now

--- resources/js/Pages/Scout/View.vue
+++ resources/js/Pages/Scout/View.vue
@@ -10,6 +10,8 @@
 <script setup>
 import MapContainer from '@/Components/MapContainer.vue';
 import { useForm } from '@inertiajs/vue3';
+import Echo from 'laravel-echo';
+import { onMounted } from 'vue';
 
 const props = defineProps({
     expac: Array,
@@ -23,4 +25,13 @@ const handleMapUpdate = function(point_data, instance_data) {
 const handlePointUpdate = function(point, mob) {
     console.log(point, mob)
 }
+
+onMounted(() => {
+    // if(props.scout.collaborator_password) {
+    //     Echo.private(`Scout.${props.scout.id}.${props.scout.collaborator_password}`)
+    //     .listen('OrderShipmentStatusUpdated', (e) => {
+    //         console.log(e.order);
+    //     });
+    // }
+})
 </script>
--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Models\Scout;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+// Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+//     return (int) $user->id === (int) $id;
+// });
+
+Broadcast::channel('Scout.{scout_id}.{collaborator_password}', function(User $user, int $scout_id, string $collab) {
+    $scout = Scout::where('id', $scout_id);
+    if($scout->collaborator_password === $collab) {
+        return true;
+    }
+    return false;
 });
