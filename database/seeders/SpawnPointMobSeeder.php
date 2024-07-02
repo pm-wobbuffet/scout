a7de1d32be1b1ee96400068021f00cd185e0581a
pm-wobbuffet <jdslone@hotmail.com>
2024-07-02 08:43:01 -0400

    Add in DT spawn points

--- database/seeders/SpawnPointMobSeeder.php
+++ database/seeders/SpawnPointMobSeeder.php
@@ -7,6 +7,25 @@ use App\Models\Zone;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+/**
+ * Handles the importing of zones from SPAWN_POINT_MOBS.csv. You can generate this file by exporting from your
+ * favorite DB engine the following query:
+ * SELECT z.id,z.name,
+    (SELECT name FROM mobs m WHERE m.zone_id = z.id AND mob_index = 1) as mob1_name,
+    (SELECT name FROM mobs m2 WHERE m2.zone_id = z.id AND mob_index = 2) as mob2_name,
+    sp.id as spawn_point_id, sp.x, sp.y,
+    valid_mobs_table.valid_mobs
+    FROM zones z
+    LEFT JOIN spawn_points sp ON(sp.zone_id = z.id)
+    LEFT JOIN (
+    SELECT sp.id, GROUP_CONCAT(mob_index ORDER BY mob_index SEPARATOR '|' ) as valid_mobs FROM spawn_points sp
+    LEFT JOIN mobs_spawn_points msp ON(msp.spawn_point_id = sp.id)
+    LEFT JOIN mobs ON (msp.mob_id = mobs.id)
+    GROUP BY sp.id
+    ) AS valid_mobs_table ON(valid_mobs_table.id = sp.id)
+ * You can then fill in the valid_mobs by using a pipe delimiter. i.e 1|2 means mobs 1 and 2 are both valid for a point, "1" would mean only mob 1, etc
+ * You can add new spawn points at the end just by inserting new lines with the appropriate format.
+ */
 class SpawnPointMobSeeder extends Seeder
 {
     /**
@@ -29,7 +48,7 @@ class SpawnPointMobSeeder extends Seeder
         if( ($handle = fopen(resource_path('csv/SPAWN_POINT_MOBS.csv'), 'r')) !== false) {
             while (($data = fgetcsv($handle, 1000, ",")) !== false) {
                 // Column 0 = zone_id
-                // Column 5 = x, Column 6 = y, 
+                // Column 5 = x, Column 6 = y,
                 // Column 7 = valid mobs in 1|2 or 1 or 2 format
                 $zId = $data[0];
                 if($zId == 'id') continue;
@@ -41,11 +60,22 @@ class SpawnPointMobSeeder extends Seeder
                     foreach($pieces as $p) {
                         $mobs[] = $zonemap[$zId][$p];
                     }
+                    /*
                     SpawnPoint::where('x', $x)
                     ->where('y', $y)
                     ->where('zone_id', $zId)
                     ->first()
                     ->valid_mobs()->sync($mobs);
+                    */
+                    $sp = SpawnPoint::firstOrCreate([
+                        'x' => $x,
+                        'y' => $y,
+                        'zone_id'   => $zId,
+                    ],[
+                        'is_active' => 1,
+                    ]
+                    );
+                    $sp->valid_mobs()->sync($mobs);
                 }
             }
         }
