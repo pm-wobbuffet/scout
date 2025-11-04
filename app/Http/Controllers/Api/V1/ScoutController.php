a4a16e78920dbf4e4665ac6eca4afe63a17ebd1f
pm-wobbuffet <6683213+pm-wobbuffet@users.noreply.github.com>
2025-11-03 19:06:01 -0500

    more APi importing

--- app/Http/Controllers/Api/V1/ScoutController.php
+++ app/Http/Controllers/Api/V1/ScoutController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\StoreScoutRequest;
 use App\Http\Requests\BulkUpdateScoutAPIRequest;
-use App\Http\Requests\StoreScoutRequest;
 use App\Http\Requests\UpdatePointOccupiedAPIRequest;
 use App\Http\Requests\UpdatePointOccupiedRequest;
 use App\Http\Requests\UpdateScoutAPIRequest;
