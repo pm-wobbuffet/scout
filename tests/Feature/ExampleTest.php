f4b51938ed547cd33287f732098d14dbd4d1ad7e
pm-wobbuffet <jdslone@hotmail.com>
2026-01-05 04:27:50 -0500

    Clean up testing folders

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,29 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia as Assert;
+
+pest()->use(RefreshDatabase::class);
+
 test('returns a successful response', function () {
     $response = $this->get('/');
 
     $response->assertStatus(200);
+});
+
+test('returns expansion data', function () {
+    $response = $this->get('/');
+    $expac = $response->inertiaProps('expac');
+    expect(sizeof($expac))->toBeGreaterThan(0);
+    // $response->assertInertia(fn(Assert $page) => $page
+    //     ->has('expac', fn(Assert $page) => $page
+    //         ->where('id', 2)));
+});
+
+test('returns valid zone data', function () {
+    $response = $this->get('/');
+    $expac = $response->inertiaProps('expac');
+    expect($expac[0])->toHaveKey('zones');
+    $zone = $expac[0]['zones'][0];
+    expect($zone)->toHaveKeys(['id', 'name', 'mobs', 'spawn_points']);
 });
