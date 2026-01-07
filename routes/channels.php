35452e481097681c70f8ff3862a802df327aeb90
pm-wobbuffet <6683213+pm-wobbuffet@users.noreply.github.com>
2026-01-07 01:09:51 -0500

    tidy up supervisor config for docker

--- routes/channels.php
+++ routes/channels.php
@@ -2,12 +2,12 @@
 
 use App\Models\Scout;
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('scouts.{scout}.{collaborator_password}', function(Scout $scout, $password) {
-    return false;
+Broadcast::channel('scouts.{scout}.{password}', function (Scout $scout, $password) {
     return $scout->collaborator_password == $password;
 });
