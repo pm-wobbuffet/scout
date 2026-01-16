409d0188b2360227f2b13fcddffd525ed0299251
pm-wobbuffet <jdslone@hotmail.com>
2026-01-16 09:09:04 -0500

    some tweaks to the bottom layout section

--- resources/js/layouts/ScoutLayout.vue
+++ resources/js/layouts/ScoutLayout.vue
@@ -2,20 +2,31 @@
     <div :style="mobStyles">
         <slot />
     </div>
-    <footer class="text-xs">
-        FINAL FANTASY XIV © 2010 - {{ dayjs().get('year') }} SQUARE ENIX CO., LTD. All Rights Reserved. Logo thanks to
-        the AHA Discord.
-        Created by Kaiden Alenko of Adamantoise.
-        Source available on <a href="https://github.com/pm-wobbuffet/scout">Github</a>.
+    <footer class="text-xs flex">
+        <div>
+            FINAL FANTASY XIV © 2010 - 2026 SQUARE ENIX CO., LTD. All Rights Reserved.
+            Created by Kaiden Alenko of Adamantoise.
+            Source on <a href="https://github.com/pm-wobbuffet/scout">Github</a>.
+        </div>
+        <div class="ml-auto">
+            <ul class="flex gap-x-2 items-center justify-center">
+                <li class="border-r">
+                    <Link :href="route('help.index')" class="px-2">Help</Link>
+                </li>
+                <li>
+                    <Link :href="route('maps.index')" class="px-2">Spawn Point Tracking</Link>
+                </li>
+            </ul>
+        </div>
     </footer>
 </template>
 
 <script setup>
 import { useUserSettings } from '@/composables/useUserSettings';
-import dayjs from 'dayjs';
+import { Link } from '@inertiajs/vue3';
 import { computed, provide } from 'vue';
 
-const { settings, updateSetting } = useUserSettings()
+const { settings } = useUserSettings()
 provide('settings', settings)
 
 const mobStyles = computed(() => {
