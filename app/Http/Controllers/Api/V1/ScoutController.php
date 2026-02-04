e8f8dc9b528c5296dd8a04f7bddfe81b05fae4dd
pm-wobbuffet <jdslone@hotmail.com>
2026-02-04 10:59:23 -0500

    Transaction attempt max

--- app/Http/Controllers/Api/V1/ScoutController.php
+++ app/Http/Controllers/Api/V1/ScoutController.php
@@ -13,6 +13,7 @@ use App\Models\Scout;
 use App\Traits\HandlesScoutUpdates;
 use App\Traits\Traits\HandlesCustomPoints;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ScoutController extends Controller
 {
@@ -78,7 +79,10 @@ class ScoutController extends Controller
 
     public function bulkUpdate(BulkUpdateScoutAPIRequest $request, Scout $scout)
     {
-        $this->createBulkUpdate($scout, $request->validated('sightings'));
+        DB::transaction(function () use ($request, $scout) {
+            $pscout = Scout::where('id', $scout->id)->lockForUpdate()->first();
+            $this->createBulkUpdate($pscout, $request->validated('sightings'));
+        }, 5);
         return response()->json([
             'scout_id'              =>  $scout->slug,
             'collaborator_password' =>  $scout->collaborator_password,
@@ -90,34 +94,39 @@ class ScoutController extends Controller
 
     public function updateOccupiedPoint(UpdatePointOccupiedAPIRequest $request, Scout $scout)
     {
-        // Get details from the request
-        $point_id = $request->validated('point_id');
-        $instance = $request->validated('instance_number');
-        $status = $request->validated('status');
-        $distance = $request->distance;
-        if ($distance && floatval($distance) > 2) {
-            return [
-                'error'             =>  'The specified point was not within range of a known A rank spawn point.',
-                'distance'          =>  $distance,
-                'closest_point'     =>  $point_id,
-                'occupied_points'   =>  $scout->occupied_points,
-            ];
-        }
-        // Grab a reference to the currently occupied point
-        $p = $scout->occupied_points;
-        if (!isset($p[$point_id])) {
-            $p[$point_id] = [];
-        }
-        // update the status of this instance point. (1 = occupied, 0 = unoccupied)
-        $p[$point_id][$instance] = $status;
-        $scout->occupied_points = $p;
-        // Make sure to credit the user if a username was supplied
-        if ($request->has('update_user') && $request->input('update_user') !== 'Anonymous') {
-            if (!in_array($request->input('update_user'), $scout->scouts)) {
-                $scout->scouts = [...$scout->scouts, $request->input('update_user')];
+        DB::transaction(function () use ($request, $scout) {
+            //
+            $scout = Scout::where('id', $scout->id)->lockForUpdate()->first();
+            // Get details from the request
+            $point_id = $request->validated('point_id');
+            $instance = $request->validated('instance_number');
+            $status = $request->validated('status');
+            $distance = $request->distance;
+            if ($distance && floatval($distance) > 2) {
+                return [
+                    'error'             =>  'The specified point was not within range of a known A rank spawn point.',
+                    'distance'          =>  $distance,
+                    'closest_point'     =>  $point_id,
+                    'occupied_points'   =>  $scout->occupied_points,
+                ];
             }
-        }
-        $scout->save();
+            // Grab a reference to the currently occupied point
+            $p = $scout->occupied_points;
+            if (!isset($p[$point_id])) {
+                $p[$point_id] = [];
+            }
+            // update the status of this instance point. (1 = occupied, 0 = unoccupied)
+            $p[$point_id][$instance] = $status;
+            $scout->occupied_points = $p;
+            // Make sure to credit the user if a username was supplied
+            if ($request->has('update_user') && $request->input('update_user') !== 'Anonymous') {
+                if (!in_array($request->input('update_user'), $scout->scouts)) {
+                    $scout->scouts = [...$scout->scouts, $request->input('update_user')];
+                }
+            }
+            $scout->save();
+        }, 5);
+
         return [
             'success'           =>  1,
             'occupied_points'   => $scout->occupied_points,
