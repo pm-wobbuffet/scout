1338db935fbc9aa4da4d782c0aa7e6d19371d060
pm-wobbuffet <jdslone@hotmail.com>
2026-02-06 02:21:34 -0500

    Add throttle to history creation

--- app/Listeners/ScoutEventsSubscriber.php
+++ app/Listeners/ScoutEventsSubscriber.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\ScoutReportModified;
 use App\Http\Resources\ScoutVersionCompactResource;
 use App\Models\ScoutVersion;
+use Carbon\Carbon;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Queue\InteractsWithQueue;
@@ -18,6 +19,12 @@ class ScoutEventsSubscriber implements ShouldQueue
      */
     public function handleScoutReportModifiedEvent(ScoutReportModified $event): void
     {
+        $lv = $event->scout->versions()->orderBy('id', 'DESC')->first();
+        // Throttle creation of report version rows to prevent excessive hits on that table
+        if ($lv !== null && $lv->created_at >= Carbon::now()->subSeconds(config('app.scout.version_history_lockout', 10))) {
+            return;
+        }
+
         $scout = $event->scout->load(['points', 'scouts', 'dead_mobs', 'custom_points', 'instances']);
         $scout->versions()->create([
             'scout_details' => $scout->toResource(ScoutVersionCompactResource::class),
